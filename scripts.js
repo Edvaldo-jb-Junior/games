15653267c3c3eca96a9c410ae7057ee455d59014
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-04-07 12:18:02 -0300

    conclusão-projeto

--- scripts.js
+++ scripts.js
@@ -2,14 +2,18 @@ const login = document.querySelector(".caixa-login")
 
 const cadastro = document.querySelector(".caixa-cadastro")
 
+const mascara = document.querySelector(".mascara-formulario")
+
 function logain(){
-    login.style.left = "80%"
+    login.style.left = "40%"
     login.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 }
 
 function novo(){
-    cadastro.style.left = "80%"
+    cadastro.style.left = "40%"
     cadastro.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 }
 
 function esconder(){
@@ -17,4 +21,5 @@ function esconder(){
     login.style.transform = "translateX(0)"
     cadastro.style.left = "-450%"
     cadastro.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
 }
